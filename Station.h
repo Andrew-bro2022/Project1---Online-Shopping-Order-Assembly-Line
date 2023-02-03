795e9389e405dbd1cc9d5a12764b71868308341b
Andy Zheng <97316979+Andrew-bro2022@users.noreply.github.com>
2023-02-02 23:07:25 -0500

    Update Station.h

--- Station.h
+++ Station.h
@@ -1,11 +1,5 @@
-//	Milestone #3
+// Station.h
 // Name:	Yuchi Zheng
-// Seneca Student ID:	025 848 151
-// Seneca email:	yzheng103@myseneca.ca
-// Date of completion: 2nd, December, 2022
-//
-// I confirm that I am the only author of this file
-// and the content was created entirely by me.
 
 #ifndef SDDS_STATION_H
 #define SDDS_STATION_H
