953e760a968cc4c1774d95fd9b8504d8be6d155f
Andy Zheng <97316979+Andrew-bro2022@users.noreply.github.com>
2023-02-02 23:05:06 -0500

    Update CustomerOrder.h

--- CustomerOrder.h
+++ CustomerOrder.h
@@ -1,11 +1,5 @@
-//	Milestone #3
-// Name:	Yuchi Zheng
-// Seneca Student ID:	025 848 151
-// Seneca email:	yzheng103@myseneca.ca
-// Date of completion: 2nd, December, 2022
-//
-// I confirm that I am the only author of this file
-// and the content was created entirely by me.
+// CustomerOrder.h
+// Name: Yuchi Zheng
 
 #ifndef SDDS_CUSTOMER_ORDER_H
 #define SDDS_CUSTOMER_ORDER_H
