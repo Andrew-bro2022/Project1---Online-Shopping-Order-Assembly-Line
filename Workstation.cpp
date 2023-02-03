84588f25ab669987ccc1edda3c8c97ff7cc47cba
Andy Zheng <97316979+Andrew-bro2022@users.noreply.github.com>
2023-02-02 23:08:48 -0500

    Update Workstation.cpp

--- Workstation.cpp
+++ Workstation.cpp
@@ -1,11 +1,6 @@
-//	Milestone #3
+//	Workstation.cpp
 // Name:	Yuchi Zheng
-// Seneca Student ID:	025 848 151
-// Seneca email:	yzheng103@myseneca.ca
-// Date of completion: 2nd, December, 2022
-//
-// I confirm that I am the only author of this file
-// and the content was created entirely by me.
+
 
 #define _CRT_SECURE_NO_WARNINGS
 #include "Workstation.h"
